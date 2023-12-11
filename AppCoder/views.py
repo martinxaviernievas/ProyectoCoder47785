6b86d15cf44bf213bc817ed36e35a8f9f5b48cef
MartinXavierNievas <100978531+martinxaviernievas@users.noreply.github.com>
2023-12-11 08:45:19 -0300

    se sigue trabajando en el proyecto

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 from AppCoder.models import Curso
 from django.http import HttpResponse
 
-def crear_curso(request):
-    curso = Curso(nombre="Python", camada=47785)
-    curso.save()
 
-    return HttpResponse(curso.nombre)
+def crear_curso(request):
+    curso = Curso(nombre="Python", camada=47783)
+    curso.save()
+    contexto = {"curso": curso}
+
+    return render(request, 'index.html', contexto)
+
+
+def show_html(request):
+    curso = Curso.objects.first()
+    contexto = {"curso": curso, "nombre": "Martin"}
+
+    return render(request, 'index.html', contexto)
